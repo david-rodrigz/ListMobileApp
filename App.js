f30130cfe1eafd789186644a3bb9e9d197914d46
David Rodriguez <davidrodriguez@u.boisestate.edu>
2024-01-25 17:13:54 -0700

    FlatList is rendering sample data

--- App.js
+++ App.js
@@ -1,20 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { FlatList, Text, SafeAreaView, StyleSheet, TouchableOpacity, TextInput } from 'react-native';
+
+// list items
+const DATA = [
+  { title: 'First Item', selected: false },
+  { title: 'Second Item', selected: false },
+  { title: 'Third Item', selected: false },
+];
+
 
 export default function App() {
+  // state array for list entries
+  const [listEntries, setlistEntries] = useState(DATA);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <FlatList style={styles.listContainer}
+        data = {listEntries}
+        renderItem = {({ item }) => <Text style={styles.listItem}>{item.title}</Text>}
+      />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignSelf: 'stretch',
+    paddingLeft: 20,
+    paddingRight: 20,
+    borderWidth: 1,
   },
+  listContainer: {
+    flex: 1,
+    alignSelf: 'stretch',
+  },
+  listItem: {
+    padding: 6,
+  },
+  selectedItem: {
+    borderWidth: 1,
+    borderBlockColor: 'blue',
+    borderRadius: 6,
+    padding: 6,
+  }
 });
